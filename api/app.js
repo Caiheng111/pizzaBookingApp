f3ae3968c996b3dac9f559f464d91829c6c16a0e
Cai heng <ianyaheng822@gmail.com>
2020-04-29 16:13:22 +1000

    add the reserve route

--- api/app.js
+++ api/app.js
@@ -1,3 +1,6 @@
+
+require("dotenv").config()
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -5,17 +8,35 @@ var logger = require('morgan');
 const mongoose = require('mongoose')
 const core = require('core')
 
+// mongoDB database connection
+var mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+var db = mongoose.connection;
 
-var indexRouter = require('./routes/index');
 var app = express();
-
+app.use(express.json());
 
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use("/availablity",require('./routes/availablityRoutes'))
+app.use("/reserve",require('./routes/reservationRoutes'))
+
+
+// database error check
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", _ => {
+  console.log("Connected to DB");
+});
 
 app.use('/', indexRouter);
 
